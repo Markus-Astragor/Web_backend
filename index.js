fd7c103f4a0783c06642013e55d6911762037a21
Markus-Astragor <kosyk.markian@gmail.com>
2023-02-28 10:15:48 +0200

    save_commit

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-//Підключення бібліотек
 const express = require('express');
-const Mongo = require('./Setup/Mongoose');
-const bodyParser = require('body-parser');
-const Theatre = require('./Api_routes/Theatre.js');
-const Sessions = require('./Api_routes/Sessions.api');
-const { default: mongoose } = require('mongoose');
 require('dotenv').config();
-
-
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+const Port = process.env.PORT; 
 
 
 
-const Setup_server = async () => {
-  await Mongo.start(process.env.MONGO_DB_URL);
+app.get('/', (req, res)=>{
+    res.send('Hi, Ostap')
+})
 
-  app.use(Theatre.router);
-  app.use(Sessions.router);
 
-  app.listen(process.env.PORT, (req, res) => {
-    console.log(`Server started on ${process.env.PORT}`)
-  })
-}
-
-Setup_server()
+app.listen(Port, () => {
+    console.log('Server was started on', Port);
+})
