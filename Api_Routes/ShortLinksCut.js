6cfafb0aac2bf0c5170262f8325731babe3a8001
Marko Kosyk <kosyk.markian@gmail.com>
2023-03-16 17:36:23 +0200

    new changes

--- Api_Routes/ShortLinksCut.js
+++ Api_Routes/ShortLinksCut.js
@@ -2,22 +2,32 @@ const { Links } = require('../models/Links');
 const Router = require('express');
 const router = Router();
 
+
+function CurrentDate() {
+  let date = new Date();
+  return date;
+}
+
+
 router.get('/shortLink/:cut', async (req, res) => {
-  const {cut} = req.params;
+  let current = CurrentDate();
+  const { cut } = req.params;
   //find cut link
-  const QueryDB ={};
+  const QueryDB = {};
   QueryDB["link.cut"] = cut;
 
 
   const docs = await Links.findOne(QueryDB);
 
-  if(!docs){
+  if (!docs) {
     return res.status(400).send('Short link wasn`t found')
   }
 
-  if(docs.link.expiredAt){
+  if (current == docs.expiredAt) {
     return res.status(400).send('Link was expired');
   }
+
+  console.log(docs.expiredAt);
 
   return res.redirect(docs.link.original);
 })
