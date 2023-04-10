0d8cd80856cad9c1cfeb0aff8ad66ff9b78b8b5f
Marko Kosyk <kosyk.markian@gmail.com>
2023-04-11 00:21:22 +0300

    new changes

---
+++ server/telegramBot.js
@@ -0,0 +1,33 @@
+const { Telegraf } = require('telegraf');
+const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
+const baseUrl = process.env.BASE_URL;
+const bot = new Telegraf(telegramBotToken);
+
+module.exports = async(app, emitter) => {
+  const router = await bot.createWebhook({
+    domain: baseUrl
+  })
+
+  bot.start((ctx) => {
+    ctx.reply('Hi, user. Use command "/login" to use your auth token \n\nExample: /login 12345-54645-564564...')
+  })
+  
+  bot.command('login', (ctx) => {
+    const [command, id] = ctx.message.text.split(' ');
+    const eventName = `login-${id}`;
+    console.log(`Try to login id:${id}`);
+    const userInfo = {
+      fistName: ctx.from.first_name,
+      lastName: ctx.from.last_name || ''
+    };
+  
+    if (!Object.values(userInfo).find(e => e.length)) {
+      userInfo.firstName = ctx.from.username || ctx.from.id;
+     }
+   
+     emitter.emit(eventName, userInfo); 
+  });
+  
+  app.use(router);
+};
+
