3e9c9243629a5a84394b8c65c5b5d5efeae63dfc
Marko Kosyk <kosyk.markian@gmail.com>
2023-04-22 01:02:42 +0300

    fix saving messages

--- server/webSocket.js
+++ server/webSocket.js
@@ -2,7 +2,7 @@ const ws = require('ws');
 const { v4: uuid } = require('uuid');
 require('dotenv').config();
 const PORT = process.env.SOCKET_PORT;
-const Message = require('./models/Message');
+const {Message} = require('./models/Message');
 
 
 const users = {};
@@ -21,8 +21,9 @@ module.exports = () => {
 
   wss.on('connection', (ws) => {
     console.log(`New connection was created`);
-    ws.on('message', (messageString) => {
+    ws.on('message',async (messageString) => {
       const message = JSON.parse(messageString);
+      console.log('messageString', messageString);
       console.log('message', message);
       if (message.event == 'first-connect') {
         const { userName } = message;
@@ -43,10 +44,11 @@ module.exports = () => {
         message.userName = 'system';
         message.text = `User with name ${userName} was connected to chat`;
       }
-      // const savedMessage = new Message({userName: userName, message: message, createdAt: new Date, messageId: uuid()});
-      // const doc = savedMessage.save();
+     const {userName, text, date} = message;
+
+      const savedMessage = new Message({userName: userName, message: text, createdAt: date, messageId: uuid()});
+      const doc = await savedMessage.save();
       
-      // res.send(doc);
       sendToAll(message);
     })
 
