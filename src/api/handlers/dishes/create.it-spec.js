78e6bf64bf9b542283149eae87c1e99255eb5cd1
DESKTOP-378FDDD\Markus <kosyk.markian@gmail.com>
2023-05-26 10:21:04 +0300

    new changes

--- src/api/handlers/dishes/create.it-spec.js
+++ src/api/handlers/dishes/create.it-spec.js
@@ -3,29 +3,34 @@ const { Dishes } = require('../../../models');
 const { createDish } = require('./create');
 describe("createDish", () => {
 
- beforeAll(async () => {
-  await mongoose.connect(process.env.MONGO_DB_URI, {
-   auth: {
-    username: process.env.MONGO_DB_LOGIN,
-    password: process.env.MONGO_DB_PASSWORD
-   }
-  });
-  console.log(`mongoose was connected`);
- });
+    beforeAll(async () => {
+        await mongoose.connect(process.env.MONGO_DB_URI, {
+            auth: {
+                username: process.env.MONGO_DB_LOGIN,
+                password: process.env.MONGO_DB_PASSWORD
+            }
+        });
+        console.log(`mongoose was connected`);
+    });
 
- it("should be saved in db", async () => {
-  const req = { body: { price: 56, isAvailable: true } };
-  const res = {
-   send: jest.fn(),
-   status: jest.fn().mockImplementation(() => res)
-  };
+    afterAll(async () => {
+        await Dishes.deleteMany();
+    })
 
-  await createDish(req, res);
-  const _id = res.send.mock.calls[0][0]._id;
-  const doc = await Dishes.findById(_id);
+    it("should be saved in db", async () => {
+        const req = { body: { price: 56, isAvailable: true } };
+        const res = {
+            send: jest.fn(),
+            status: jest.fn().mockImplementation(() => res)
+        };
 
-  expect(doc).not.toBeNull();
-  expect(doc.price).toBe(56);
-  expect(doc.isAvailable).toBe(true);
- });
+        await createDish(req, res);
+        const _id = res.send.mock.calls[0][0]._id;
+        const doc = await Dishes.findById(_id);
+
+        expect(doc).not.toBeNull();
+        expect(doc.price).toBe(56);
+        expect(doc.isAvailable).toBe(true);
+    });
 });
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-require('dotenv').config({path: '../.env'});
+require('dotenv').config();
 const cors = require('cors');
 const path = require('path');
 const loggerMiddleware = require('./middleware/logger');
